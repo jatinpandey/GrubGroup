8d2f348608ba4df15e26a594c433aa99a02b3b48
Jatin <jatinp@wondersingle-lm.local>
2015-10-08 22:36:28 -0700

    Initial Commit

---
+++ GrubGroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrubGroup
+//
+//  Created by Jatin Pandey on 10/8/15.
+//  Copyright © 2015 Jatin Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
